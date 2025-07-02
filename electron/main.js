be672374f2d29dcc0627b93e8db4af499e4ab58f
rayenomr123 <rayenomri23p@gmail.com>
2025-07-02 17:21:45 +0100

    Electron + React project structure

---
+++ electron/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false,
+    },
+  });
+
+  win.maximize();
+
+  // Load the Vite dev server in development, or the built index.html in production
+  const isDev = process.env.NODE_ENV !== 'production';
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  Menu.setApplicationMenu(null);
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
